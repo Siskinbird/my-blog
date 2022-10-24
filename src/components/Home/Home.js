f0c37fedc0e44aa8776326c3eb8e294d7387f9c0
Dmitrii Chizhov <dimachig@gmail.com>
2022-10-24 14:05:55 +0300

    add: deleteBlog function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,25 @@
 import {useState} from "react";
+import BlogList from "../BlogList/BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body: 'lorem ipsum...', author: 'Dmitry', id: 1},
-        {title: 'Welcome party', body: 'lorem ipsum...', author: 'Ivan', id: 2},
+        {title: 'Welcome party', body: 'lorem ipsum...', author: 'Gleb', id: 2},
         {title: 'World is ower', body: 'lorem ipsum...', author: 'Gleb', id: 3}
-    ])
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs)
+    }
+
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs}
+                      title='All blogs'
+                      handleDelete={handleDelete}/>
+            {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'Gleb')}*/}
+            {/*          title='Gleb blog'/>*/}
         </div>
     );
 }
