979a88e1a7187d3d0c72ba59a550050c3596181a
Dmitrii Chizhov <dimachig@gmail.com>
2022-10-24 10:58:32 +0300

    add: simple navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  const title = 'Welcome to te new blog';
+  const likes = 50;
+  const person = {
+    name: 'Dmitry',
+    age: 36
+  }
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} time</p>
       </div>
     </div>
   );
