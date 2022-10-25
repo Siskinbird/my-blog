e36c419acd36991c93e30da57c052eacfc238239
Dmitrii Chizhov <dimachig@gmail.com>
2022-10-25 18:47:50 +0300

    add: delete blog function

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,9 +1,19 @@
-import {useParams} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 import useFetch from "../../useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams()
     const {data: blog, isPending, error} = useFetch(`http://localhost:8000/blogs/${id}`)
+    const history = useHistory()
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${blog.id}`, {
+            method: 'DELETE',
+        }).then(() => {
+            history.push('/')
+        })
+    }
+
     return(
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -13,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <h3>Written by - {blog.author}</h3>
                     <p>{blog.body}</p>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
