9ce1cd9a3acdeaeb3126edc4f98c28f724e00375
Dmitrii Chizhov <dimachig@gmail.com>
2022-10-25 10:57:16 +0300

    add: Fetch details component

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,10 +1,20 @@
 import {useParams} from "react-router-dom";
+import useFetch from "../../useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams()
+    const {data: blog, isPending, error} = useFetch(`http://localhost:8000/blogs/${id}`)
     return(
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <h3>Written by - {blog.author}</h3>
+                    <p>{blog.body}</p>
+                </article>
+            )}
         </div>
     );
 }
