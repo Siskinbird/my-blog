49b76f31fd85dc4776f32a41a359b7ba2f2bd884
Dmitrii Chizhov <dimachig@gmail.com>
2022-10-25 10:28:16 +0300

    add: CleanUp useEffect

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,21 +2,28 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 const useFetch = (url) => {
+
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        axios.get(url)
+        const abortCont = new AbortController();
+        axios.get(url, {signal: abortCont.signal})
             .then((res) => {
                 setData(res.data);
                 setIsPending(false);
                 setError(null);
             }).catch((error) => {
-            setIsPending(false);
-            setError(error.message);
-        })
-    }, [])
+                if (error.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
+                    setIsPending(false);
+                    setError(error.message);
+                }
+            });
+        return () => abortCont.abort();
+    }, [url])
     return {data, isPending, error}
 }
 
